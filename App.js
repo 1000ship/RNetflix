ab421f5448736d8fd554f4b86b805f87c8294a3c
1000ship <cjstjdgur1234@gmail.com>
2020-06-29 01:54:34 +0900

    Init RN Project

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { AppLoading } from 'expo';
+
+const cacheImages = (images) => {
+  images.map( image => {
+    if(typeof image === "string"){
+      return Image.prefetch(image)
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+
+    }
+  })
+}
 
 export default function App() {
-  return (
+  const [isReady, setIsReady] = useState(false)
+  const loadAssets = async () => {
+    const images = cacheImages
+  }
+  const onFinish = () => {
+    setIsReady(true)
+  }
+  return !isReady ? <AppLoading startAsync={loadAssets} onFinish={onFinish} onError={(e)=>console.error(e)}/> : (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!??d</Text>
+
       <StatusBar style="auto" />
     </View>
   );
