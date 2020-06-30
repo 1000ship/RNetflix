6486118a4fc7ea9cbb144db618fc68734e565cdd
1000ship <cjstjdgur1234@gmail.com>
2020-06-30 16:42:00 +0900

    Use Ionicon for tab-icon in Tabs.js

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -4,6 +4,8 @@ import Movies from '../screens/Movies';
 import Tv from '../screens/Tv';
 import Search from '../screens/Search';
 import Favs from '../screens/Favs';
+import { Ionicons } from '@expo/vector-icons';
+import { Platform } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,7 +19,29 @@ export default ( {navigation, route} ) => {
     } )
   }, [ route ] );
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={ ({route}) => ({
+        tabBarIcon: ({focused}) => {
+          let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+          if(route.name === "Movies"){
+            iconName += "film"
+          } else if(route.name === "TV"){
+            iconName += "tv"
+          } else if(route.name === "Search"){
+            iconName += "search"
+          } else if(route.name === "Favs"){
+            iconName += "heart"
+          }
+          return <Ionicons name={iconName} color={focused ? "white" : "grey"} size={26}/>
+        }
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: "black",
+          borderTopColor: "black",
+        }
+      }}>
       <Tab.Screen name="Movies" component={Movies} />
       <Tab.Screen name="TV" component={Tv} />
       <Tab.Screen name="Search" component={Search} />
