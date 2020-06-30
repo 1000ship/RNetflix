eba56b751afb59558b417c59b3f4a6ba366892c9
1000ship <cjstjdgur1234@gmail.com>
2020-06-30 16:12:00 +0900

    Change Tabs titleName by navigation, route params

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Movies from '../screens/Movies';
 import Tv from '../screens/Tv';
 import Search from '../screens/Search';
 import Favs from '../screens/Favs';
 
-
 const Tab = createBottomTabNavigator();
 
-export default () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="Tv" component={Tv} />
-    <Tab.Screen name="Search" component={Search} />
-    <Tab.Screen name="Favs" component={Favs} />
-  </Tab.Navigator>
-);
+const getHeaderName = (route) => route?.state?.routeNames[route?.state?.index] || "Movies"
+
+export default ( {navigation, route} ) => {
+  useLayoutEffect( ()=>{
+    navigation?.setOptions( {
+       title: getHeaderName(route)
+    } )
+  }, [ route ] );
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Movies" component={Movies} />
+      <Tab.Screen name="Tv" component={Tv} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Favs" component={Favs} />
+    </Tab.Navigator>
+  )
+};
