0ebd4a222a67fee277aeaf3ec5e1b51c192456d7
1000ship <cjstjdgur1234@gmail.com>
2020-07-08 17:47:19 +0900

    Finish Favs, Card's animation

--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -29,37 +29,70 @@ const Poster = styled.Image`
 
 const FavsPresenter = ({ results }) => {
   const [topIndex, setTopIndex] = useState(0);
+  const nextCard = () => setTopIndex((currentValue) => currentValue + 1);
   const position = new Animated.ValueXY();
   const panResponse = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (evt, { dx, dy }) => {
       position.setValue({ x: dx, y: dy });
     },
-    onPanResponderRelease: () => {
-      Animated.spring(position, {
-        toValue: { x: 0, y: 0 },
-        useNativeDriver: true,
-        bounciness: 10,
-      }).start();
+    onPanResponderRelease: (evt, { dx, dy }) => {
+      if (dx >= 150) {
+        Animated.spring(position, {
+          toValue: { x: WIDTH + 100, y: dy },
+          useNativeDriver: true,
+        }).start(nextCard);
+      } else if (dx <= -150) {
+        Animated.spring(position, {
+          toValue: { x: -WIDTH - 100, y: dy },
+          useNativeDriver: true,
+        }).start(nextCard);
+      } else {
+        Animated.spring(position, {
+          toValue: { x: 0, y: 0 },
+          useNativeDriver: true,
+          bounciness: 10,
+        }).start();
+      }
     },
   });
   const rotationValues = position.x.interpolate({
-    inputRange: [-100, 0, 100],
-    outputRange: ['-5deg', '0deg', '5deg'],
+    inputRange: [-255, 0, 255],
+    outputRange: ["-8deg", "0deg", "8deg"],
+    extrapolate: "clamp",
+  });
+  const secondCardOpacity = position.x.interpolate({
+    inputRange: [-255, 0, 255],
+    outputRange: [1, 0.2, 1],
+    extrapolate: "clamp",
+  });
+  const secondCardScale = position.x.interpolate({
+    inputRange: [-255, 0, 255],
+    outputRange: [1, 0.8, 1],
     extrapolate: "clamp",
   });
   return (
     <Container>
-      {results.reverse().map((result, index) => {
-        return (
+      {results.map((result, index) => {
+        return index < topIndex ? null : (
           <Card
             style={
               index === topIndex
                 ? {
                     zIndex: 1,
-                    transform: [{rotate: rotationValues}, ...position.getTranslateTransform()],
+                    transform: [
+                      { rotate: rotationValues },
+                      ...position.getTranslateTransform(),
+                    ],
+                  }
+                : index === topIndex + 1
+                ? {
+                    opacity: secondCardOpacity,
+                    transform: [{ scale: secondCardScale }],
+                    zIndex: -index,
                   }
                 : {
+                    opacity: 0,
                     zIndex: -index,
                   }
             }
